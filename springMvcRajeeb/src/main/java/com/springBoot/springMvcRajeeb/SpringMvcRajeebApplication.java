711bc5173136bd11132f09a59538a50e814b79a9
Rajeeb Pati <rajeeb.pati@subex.com>
2021-06-12 15:49:51 +0530

    Creation of Beer project

---
+++ springMvcRajeeb/src/main/java/com/springBoot/springMvcRajeeb/SpringMvcRajeebApplication.java
@@ -0,0 +1,14 @@
+package com.springBoot.springMvcRajeeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRajeebApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRajeebApplication.class, args);
+	}
+
+}
---
+++ springMvcRajeeb/src/test/java/com/springBoot/springMvcRajeeb/SpringMvcRajeebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.springMvcRajeeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRajeebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
